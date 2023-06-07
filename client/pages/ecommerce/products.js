f8f8409a1dfd4fb9fe2cfd6d3658283f08069343
V ranjith <ranjithsvelmurugan@gmail.com>
2023-06-07 18:35:52 +0530

    help&support

--- client/pages/ecommerce/products.js
+++ client/pages/ecommerce/products.js
@@ -1,40 +1,4 @@
 import * as React from "react";
-import { Box } from "@mui/material";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import { Typography } from "@mui/material";
-import PropTypes from "prop-types";
-import { useTheme } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableHead from "@mui/material/TableHead";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableFooter from "@mui/material/TableFooter";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import IconButton from "@mui/material/IconButton";
-import FirstPageIcon from "@mui/icons-material/FirstPage";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import LastPageIcon from "@mui/icons-material/LastPage";
-import Tooltip from "@mui/material/Tooltip";
-import DeleteIcon from "@mui/icons-material/Delete";
-import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import Backdrop from "@mui/material/Backdrop";
-import Button from "@mui/material/Button";
-import Fade from "@mui/material/Fade";
-import Modal from "@mui/material/Modal";
-import TextField from "@mui/material/TextField";
-import AddIcon from "@mui/icons-material/Add";
-import ClearIcon from "@mui/icons-material/Clear";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Link from 'next/link';
 import styles from '@/styles/PageTitle.module.css'
 
@@ -44,13 +8,31 @@ export default function Products() {
     <>
       {/* Page title */}
       <div className={styles.pageTitle}>
-        <h1>Products</h1>
+        
+        <h1 className={styles.pageh}>Help & Support</h1>
+        
         <ul>
           <li>
             <Link href="/">Dashboard</Link>
           </li>
-          <li>Products</li>
+          <li>Help & support</li>
         </ul>
+      </div>
+      <div>
+      <h2 className={styles.heading}>Contact Information</h2>
+      <hr className={styles.line}></hr>
+      </div>
+      <div>
+      <ul className={styles.list}>
+      <li><h2 className={styles.emailheading}>Email</h2></li>
+      <li><h3 className={styles.email}>info@humcen.com</h3></li>
+      </ul>
+      <hr className={styles.line}></hr>
+      <div>
+      <h2 className={styles.heading}>Frequently Asked Qusetions</h2>
+      <hr className={styles.line}></hr>
+      </div>
+      
       </div>
     </>
   );
