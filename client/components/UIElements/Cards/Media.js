a4034e9738056accae102cb4a677efd62ae7ef53
susindhar21 <susindhar02@gmail.com>
2023-06-08 15:31:20 +0530

    Created new page for Patent Services, added cards and a new swtich style

--- client/components/UIElements/Cards/Media.js
+++ client/components/UIElements/Cards/Media.js
@@ -4,32 +4,91 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
+import Grid from "@mui/material/Grid";
 import Typography from '@mui/material/Typography';
+import Switch from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
-export default function Media() {
+const IOSSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 38,
+  height: 20,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+        opacity: 1,
+        border: 0,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: '#33cf4d',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color:
+        theme.palette.mode === 'light'
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 18,
+    height: 17,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === 'light' ? '#F14242' : '#27AE60',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
+
+
+export default function Media(service) {
   return (
-    <Card sx={{ mb: '15px' }}>
+    <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
+    <Card sx={{ mb: '15px'}} style={{}}>
       <CardMedia
         sx={{ height: 140 }}
-        image="/images/contemplative-reptile.jpg"
-        title="green iguana"
+        style= {{marginTop: 10, marginBlock: 10 ,borderRadius: 20}}
+        image= {service.image}
+        title= {service.title}
       />
-      
+      <FormControlLabel
+        style = {{marginLeft: 10, position: "relative", float: "right"}}
+        control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
+      />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        <Typography gutterBottom variant="h6" component="div">
+          {service.title}
         </Typography>
 
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+        <Typography variant="body2" color="text.secondary" style={{hyphens: "auto"}}>
+          {service.desc.length > 90 ?
+            `${service.desc.substring(0, 90)}...` : service.desc
+          }
         </Typography>
       </CardContent>
-
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      
     </Card>
+    </Grid>
   );
 }
--- client/pages/apps/file-manager/index.js
+++ client/pages/apps/file-manager/index.js
@@ -6,24 +6,53 @@ import LeftSidebar from '@/components/Apps/FileManager/LeftSidebar';
 import MyDrive from '@/components/Apps/FileManager/MyDrive';
 import RecentFiles from '@/components/Apps/FileManager/RecentFiles';
 import Files from '@/components/Apps/FileManager/Files';
+import Card from '@/components/UIElements/Cards/Media';
 
+const serviceList = [
+  {
+    image: '/images/patent_consult.png',
+    title: "Patent Consultation",
+    desc: "Maximize the value of your invention with expert Patent Consultation from Us"
+  },
+  {
+    image: '/images/patent_drafting.png',
+    title: "Patent Drafting",
+    desc: "Transform your ideas into strong patents with our expert Patent Drafting service"
+  },
+  {
+    image: '/images/patent_filing.png',
+    title: "Patent Filing",
+    desc: "Secure your innovative ideas with our hassle-free patent filing service. Let us handle the complexities of the patent application process"
+  },
+  {
+    image: '/images/patent_search.png',
+    title: "Patent Search",
+    desc: "Uncover the potential of your invention with our in-depth Patent Search service."
+  }
+]
 export default function FileManager() {
   return (
     <>
       {/* Page title */}
       <div className={styles.pageTitle}>
-        <h1>File Manager</h1>
+        <h1>My Patent Services</h1>
         <ul>
           <li>
             <Link href="/">Dashboard</Link>
           </li>
           <li>
-            File Manager
+            My Patent Services
           </li>
         </ul>
       </div>
 
-      
+      <Grid
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 1, md: 1, lg: 1, xl: 2 }}
+      >
+        {serviceList.map(Card)}
+      </Grid>
     </>
   );
 }
