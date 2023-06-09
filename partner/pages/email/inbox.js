b03c6738ab83fa9eb4450afdbd7a1c8b87259881
susindhar21 <susindhar02@gmail.com>
2023-06-09 10:12:38 +0530

    Admin, Partner and User modules created

---
+++ partner/pages/email/inbox.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Grid from "@mui/material/Grid";
+import Link from 'next/link';
+import styles from '@/styles/PageTitle.module.css'
+import LeftSidebar from '@/components/Email/LeftSidebar';
+import EmailLists from '@/components/Email/EmailLists';
+import RecentOrders from '@/components/Dashboard/eCommerce/RecentOrders';
+ 
+export default function Inbox() {
+  return (
+    <>
+      {/* Page title */}
+      <div className={styles.pageTitle}>
+        <ul>
+          <li>
+            <Link href="/">Dashboard</Link>
+          </li>
+          <li>Job Order</li>
+        </ul>
+      </div>
+      <RecentOrders/>
+
+    </>
+  );
+}
---
+++ user/components/_App/LeftSidebar/SidebarData.js
@@ -0,0 +1,93 @@
+import React from "react";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import GridViewIcon from "@mui/icons-material/GridView";
+import LayersIcon from "@mui/icons-material/Layers";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import LockIcon from "@mui/icons-material/Lock";
+import SettingsIcon from "@mui/icons-material/Settings";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import AddchartIcon from "@mui/icons-material/Addchart";
+import CopyAllIcon from "@mui/icons-material/CopyAll";
+import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
+import ViewQuiltIcon from "@mui/icons-material/ViewQuilt";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+
+export const SidebarData = [
+  {
+    title: "Dashboard",
+    path: "/",
+    icon: <GridViewIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+  },
+  {
+    title: "Patent Services",
+    path: "/apps/file-manager/",
+    icon: <LayersIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+
+  },
+  {
+    title: "Job Order",
+    path: "/email/inbox/",
+    icon: <MailOutlineIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+  },
+  {
+    title: "My Income",
+    path: "/contact-list/",
+    icon: <PostAddIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+  },
+  {
+    title: "Why Us?",
+    path: "/projects/",
+    icon: <CopyAllIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+  },
+  
+  {
+    title: "Help & Support",
+    path: "/ecommerce/products/",
+    icon: <ShoppingCartCheckoutIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+  },
+  {
+    title: "Settings",
+    path: "/settings/account/",
+    icon: <SettingsIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+
+    subNav: [
+      {
+        title: "Account",
+        path: "/settings/account/",
+      },
+      {
+        title: "Security",
+        path: "/settings/security/",
+      },
+      {
+        title: "Privacy Policy",
+        path: "/settings/privacy-policy/",
+      },
+      {
+        title: "Terms & Conditions",
+        path: "/pages/terms-conditions/",
+      },
+      {
+        title: "Logout",
+        path: "/authentication/logout/",
+      },
+    ],
+  },
+];
