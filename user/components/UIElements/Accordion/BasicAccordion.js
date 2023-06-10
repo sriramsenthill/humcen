ed2ccbe16069624fbe622e08cbe6f2c5c5ee949f
susindhar21 <susindhar02@gmail.com>
2023-06-10 12:34:14 +0530

    Help & Support page of User module completed

--- user/components/UIElements/Accordion/BasicAccordion.js
+++ user/components/UIElements/Accordion/BasicAccordion.js
@@ -1,75 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import { Typography } from "@mui/material";
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 export default function BasicAccordion() {
+  const [expanded1, setExpanded1] = useState(false);
+  const [expanded2, setExpanded2] = useState(false);
+  const [expanded3, setExpanded3] = useState(false);
+
+  const handleAccordion1Change = () => {
+    setExpanded1(!expanded1);
+  };
+  const handleAccordion2Change = () => {
+    setExpanded2(!expanded2);
+  };
+  const handleAccordion3Change = () => {
+    setExpanded3(!expanded3);
+  };
+
   return (
     <>
-      <Card
-        sx={{
-          boxShadow: "none",
-          borderRadius: "10px",
-          p: "25px",
-          mb: "15px",
-        }}
+      <Accordion
+        expanded={expanded1}
+        onChange={handleAccordion1Change}
+        style={{ border: "none" }}
       >
-        <Typography
-          as="h3"
-          sx={{
-            fontSize: 18,
-            fontWeight: 500,
-            mb: '10px'
-          }}
+        <AccordionSummary
+          expandIcon={
+            expanded1 ? (
+              <RemoveIcon style={{ color: "#00ACF6" }} />
+            ) : (
+              <AddIcon style={{ color: "#00ACF6" }} />
+            )
+          }
+          aria-controls="panel1a-content"
+          id="panel1a-header"
         >
-          Basic Accordion
-        </Typography>
-
-        <Accordion className="bg-black">
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography component="h1" fontWeight="500">Accordion 1</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-
-        <Accordion className="bg-black">
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2a-content"
-            id="panel2a-header"
-          >
-            <Typography component="h1" fontWeight="500">Accordion 2</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-
-        <Accordion disabled className="bg-black">
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3a-content"
-            id="panel3a-header"
-          >
-            <Typography component="h1" fontWeight="500">Disabled Accordion</Typography>
-          </AccordionSummary>
-        </Accordion>
-      </Card>
+          <Typography component="h1" fontWeight="500">
+            Diam maecenas ultricies mi eget mauris pharetra et ultrices neque?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        className="bg-black"
+        expanded={expanded2}
+        onChange={handleAccordion2Change}
+      >
+        <AccordionSummary
+          expandIcon={
+            expanded2 ? (
+              <RemoveIcon style={{ color: "#00ACF6" }} />
+            ) : (
+              <AddIcon style={{ color: "#00ACF6" }} />
+            )
+          }
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography component="h1" fontWeight="500">
+            Vitae elementum curabitur vitae nunc. Eget dolor morbi non arcu
+            risus quis. Varius duis at consectetur lorem donec?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Varius duis at consectetur lorem donec massa sapien faucibus?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        className="bg-black"
+        expanded={expanded3}
+        onChange={handleAccordion3Change}
+      >
+        <AccordionSummary
+          expandIcon={
+            expanded3 ? (
+              <RemoveIcon style={{ color: "#00ACF6" }} />
+            ) : (
+              <AddIcon style={{ color: "#00ACF6" }} />
+            )
+          }
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography component="h1" fontWeight="500">
+            Varius duis at consectetur lorem donec massa sapien faucibus?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </>
   );
 }
--- user/pages/ecommerce/products.js
+++ user/pages/ecommerce/products.js
@@ -1,39 +1,54 @@
 import * as React from "react";
-import Link from 'next/link';
-import styles from '@/styles/PageTitle.module.css'
+import Link from "next/link";
+import styles from "@/styles/PageTitle.module.css";
+import BasicAccordion from "@/components/UIElements/Accordion/BasicAccordion";
+import { Card } from "@mui/material";
 
 export default function Products() {
-
   return (
     <>
-      {/* Page title */}
-      <div className={styles.pageTitle}>
-        
-        <h1 className={styles.pageh}>Help & Support</h1>
-        
-        <ul>
-          <li>
-            <Link href="/">Dashboard</Link>
-          </li>
-          <li>Help & support</li>
-        </ul>
-      </div>
-      <div>
-      <h2 className={styles.heading}>Contact Information</h2>
-      <hr className={styles.line}></hr>
-      </div>
-      <div>
-      <ul className={styles.list}>
-      <li><h2 className={styles.emailheading}>Email</h2></li>
-      <li><h3 className={styles.email}>info@humcen.com</h3></li>
-      </ul>
-      <hr className={styles.line}></hr>
-      <div>
-      <h2 className={styles.heading}>Frequently Asked Qusetions</h2>
-      <hr className={styles.line}></hr>
-      </div>
-      
-      </div>
+      <Card style={{ padding: "2em" }}>
+        {/* Page title */}
+        <div className={styles.pageTitle}>
+          <h1 className={styles.pageh}>Help & Support</h1>
+
+          <ul>
+            <li>
+              <Link href="/">Dashboard</Link>
+            </li>
+            <li>Help & support</li>
+          </ul>
+        </div>
+        <div>
+          <h2 className={styles.heading}>Contact Information</h2>
+          <hr className={styles.line}></hr>
+        </div>
+        <div>
+          <ul className={styles.list}>
+            <li>
+              <h2 className={styles.emailheading}>Email</h2>
+            </li>
+            <li>
+              <p className={styles.email}>info@humcen.com</p>
+            </li>
+          </ul>
+          <hr className={styles.line} style={{ width: "70%" }}></hr>
+          <ul className={styles.list}>
+            <li>
+              <h2 className={styles.emailheading}>Phone</h2>
+            </li>
+            <li>
+              <p className={styles.email}>+91 98765-43210</p>
+            </li>
+          </ul>
+          <hr className={styles.line} style={{ width: "70%" }}></hr>
+          <div>
+            <h2 className={styles.heading}>Frequently Asked Qusetions</h2>
+            <hr className={styles.line}></hr>
+            <BasicAccordion />
+          </div>
+        </div>
+      </Card>
     </>
   );
 }
