b064894a542349db1d86262209eb71de316504dd
V ranjith <ranjithsvelmurugan@gmail.com>
2023-06-12 13:50:25 +0530

    link added for patent serivices user

---
+++ user/pages/apps/file-manager/consultation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ConsultationPage = () => {
+  return (
+    <div>
+      <h2>Patent Consultation</h2>
+      <p>
+        Maximize the value of your invention with expert Patent Consultation from Us. Our team of experienced professionals will provide you with valuable insights and guidance regarding your patent-related queries and concerns.
+      </p>
+      {/* Add consultation page content here */}
+    </div>
+  );
+};
+
+export default ConsultationPage;
--- user/pages/apps/file-manager/index.js
+++ user/pages/apps/file-manager/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '@/styles/PageTitle.module.css'
+import { useRouter } from 'next/router';
+import styles from '@/styles/PageTitle.module.css';
 import Grid from "@mui/material/Grid";
 import LeftSidebar from '@/components/Apps/FileManager/LeftSidebar';
 import MyDrive from '@/components/Apps/FileManager/MyDrive';
@@ -12,25 +13,32 @@ const serviceList = [
   {
     image: '/images/patent_consult.png',
     title: "Patent Consultation",
-    desc: "Maximize the value of your invention with expert Patent Consultation from Us"
+    desc: "Maximize the value of your invention with expert Patent Consultation from Us",
+    link: "/apps/file-manager/consultation"
   },
   {
     image: '/images/patent_drafting.png',
     title: "Patent Drafting",
-    desc: "Transform your ideas into strong patents with our expert Patent Drafting service"
+    desc: "Transform your ideas into strong patents with our expert Patent Drafting service",
+    link: "/apps/file-manager/drafting"
   },
   {
     image: '/images/patent_filing.png',
     title: "Patent Filing",
-    desc: "Secure your innovative ideas with our hassle-free patent filing service. Let us handle the complexities of the patent application process"
+    desc: "Secure your innovative ideas with our hassle-free patent filing service. Let us handle the complexities of the patent application process",
+    link: "/apps/file-manager/filing"
   },
   {
     image: '/images/patent_search.png',
     title: "Patent Search",
-    desc: "Uncover the potential of your invention with our in-depth Patent Search service."
+    desc: "Uncover the potential of your invention with our in-depth Patent Search service.",
+    link: "/apps/file-manager/search"
   }
-]
+];
+
 export default function FileManager() {
+  const router = useRouter();
+
   return (
     <>
       {/* Page title */}
@@ -51,7 +59,13 @@ export default function FileManager() {
         rowSpacing={1}
         columnSpacing={{ xs: 1, sm: 1, md: 1, lg: 1, xl: 2 }}
       >
-        {serviceList.map(Card)}
+        {serviceList.map((service, index) => (
+          <Grid item xs={12} sm={6} md={6} lg={4} xl={3} key={index}>
+            <Link href={service.link}>
+                <Card {...service} />
+            </Link>
+          </Grid>
+        ))}
       </Grid>
     </>
   );
