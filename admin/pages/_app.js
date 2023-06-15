ff3ccb82b4b30a586f398cbac3af4dae85a5ac36
Senthilnathan S <macbook@Senthilnathans-MacBook-Air.local>
2023-06-15 16:31:26 +0530

    font changed for all 3, npm install all 3

--- admin/pages/_app.js
+++ admin/pages/_app.js
@@ -3,6 +3,8 @@ import '../styles/remixicon.css'
 import 'react-tabs/style/react-tabs.css';
 import "swiper/css";
 import "swiper/css/bundle";
+import {Inter} from '@next/font/google'
+
 
 // Chat Styles
 import '../styles/chat.css'
@@ -20,16 +22,21 @@ import theme from '../styles/theme'
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import Layout from "@/components/_App/Layout";
 
+const inter = Inter({
+  subsets: ['latin'],
+  weight:['400', '700'],
+})
+
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <main className={inter.className}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
-    </>
+      </main>
   );
 }
 
