623cb219e3c4065eb9640a51bd901265a1fcae8d
susindhar21 <susindhar02@gmail.com>
2023-06-23 13:28:08 +0530

    Deleted unwanted ui elements in user module

--- user/pages/index.js
+++ user/pages/index.js
@@ -1,20 +1,6 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import Link from "next/link";
-import styles from "@/styles/PageTitle.module.css";
-import Features from "@/components/Dashboard/eCommerce/Features";
-import Ratings from "@/components/Dashboard/eCommerce/Ratings";
-import AudienceOverview from "@/components/Dashboard/eCommerce/AudienceOverview";
-import VisitsByDay from "@/components/Dashboard/eCommerce/VisitsByDay";
 import Impressions from "@/components/Dashboard/eCommerce/Impressions";
-import ActivityTimeline from "@/components/Dashboard/eCommerce/ActivityTimeline";
-import Performance from "@/components/Dashboard/eCommerce/Performance";
-import SalesByCountries from "@/components/Dashboard/eCommerce/SalesByCountries";
-import NewCustomers from "@/components/Dashboard/eCommerce/NewCustomers";
-import RecentOrders from "@/components/Dashboard/eCommerce/RecentOrders";
-import TeamMembersList from "@/components/Dashboard/eCommerce/TeamMembersList";
-import BestSellingProducts from "@/components/Dashboard/eCommerce/BestSellingProducts";
-import LiveVisitsOnOurSite from "@/components/Dashboard/eCommerce/LiveVisitsOnOurSite";
 import BasicTabs from "@/components/UIElements/Tabs/BasicTabs";
 
 export default function eCommerce() {
