54078a21ecdb0d984c4edc717695ba9a060ad423
susindhar21 <susindhar02@gmail.com>
2023-06-26 12:44:34 +0530

    Changed path and renamed files for my-patent-nft page

--- user/components/_App/LeftSidebar/SidebarData.js
+++ user/components/_App/LeftSidebar/SidebarData.js
@@ -33,7 +33,7 @@ export const SidebarData = [
   },
   {
     title: "My Patent NFTs",
-    path: "/ecommerce/product-details/",
+    path: "/my-patent-nft/",
     icon: <RequestPageIcon />,
     iconClosed: <KeyboardArrowRightIcon />,
     iconOpened: <KeyboardArrowDownIcon />,
